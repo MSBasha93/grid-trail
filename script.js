97a18f433007f62b8dd6e3bb68f8953f88a4aea8
MBasha <55190972+MSBasha93@users.noreply.github.com>
2024-12-04 01:34:49 +0200

    Create hardcoded 16*16 grid
    Each row is NOT 16

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-const container = document.querySelector("container");
+const container = document.querySelector(".container");
+let numberOfPixels= 16*16;
 
-for(let i=0 ; i<16*16;i++){
+// Create grid pixels
+for(let i=0 ; i<numberOfPixels;i++){
     const elem=document.createElement("div");
     elem.classList.add("child");
+    elem.style.width= 960/16 + "px";
+    elem.style.height=960/16 + "px";
     container.appendChild(elem);
 }
-
-
-
